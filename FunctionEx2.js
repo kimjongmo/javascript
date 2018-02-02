ee59d868c6f4be8c1663b42c671074f6ca6f4566
kimjongmo <kfmd1008@naver.com>
2018-02-02 15:41:00 +0900

    Function

---
+++ FunctionEx2.js
@@ -0,0 +1,7 @@
+const takeOrder = (topping,crustType) => {
+  	console.log(`Order: thin ${crustType} pizza topped with ${topping}`);
+}
+
+takeOrder('pineapple','Cheese');
+takeOrder('mushroom','rye');
+takeOrder('sausage','Cheese');
