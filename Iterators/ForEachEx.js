ed8ca0c63ea5587e707c249ebf90b631fd150e7a
kimjongmo <kfmd1008@naver.com>
2018-02-07 15:21:54 +0900

    Iterators

---
+++ Iterators/ForEachEx.js
@@ -0,0 +1,9 @@
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+fruits.forEach(function(fruit){
+  console.log(`I want to eat a ${fruit}`);
+});
+
+fruits.forEach(fruit=>console.log(`I want to eat a ${fruit}`));
