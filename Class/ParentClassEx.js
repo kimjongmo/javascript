8d90d9e1599a7e0ce34a4681044866bb9f65916a
kimjongmo <kfmd1008@naver.com>
2018-02-11 23:39:35 +0900

    class

---
+++ Class/ParentClassEx.js
@@ -0,0 +1,16 @@
+class HospitalEmployee{
+  constructor(name){
+    this._name = name;
+    this._remainingVacationDays=20;
+  }
+  get name(){
+    return this._name;
+  }
+  get remainingVacationDays(){
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff){
+    this._remainingVacationDays=
+      this._remainingVacationDays-daysOff;
+  }
+}
