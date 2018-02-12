3427ae9386402e4341c602e8e80ef750b8d1bb2f
kimjongmo <kfmd1008@naver.com>
2018-02-12 17:57:13 +0900

    Browser Compatibility

---
+++ Browser Compatibility/ErrorRunES6.js
@@ -0,0 +1,11 @@
+var pasta = "Spaghetti";
+
+const meat = "Pancetta";
+
+let sauce = "Eggs and cheese";
+
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/*
+syntaxError :'const' 'let'
+*/
